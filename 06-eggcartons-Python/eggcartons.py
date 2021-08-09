0d6cfa3f123d12159e52e7986ac9e54faf771abb
SriChandan2498 <chandan.zna7@gmail.com>
2021-08-09 14:44:27 +0530

    passed all test cases

--- 06-eggcartons-Python/eggcartons.py
+++ 06-eggcartons-Python/eggcartons.py
@@ -6,4 +6,12 @@
 
 def fun_eggcartons(eggs):
 	# your code goes here
-	return 1
+	if(eggs == 0):
+		return 0
+	elif(eggs <= 12):
+		return 1
+	else:
+		if(eggs % 12 == 0):
+			return eggs//12
+		else:
+			return eggs//12 + 1
