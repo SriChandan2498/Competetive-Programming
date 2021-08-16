1078010aad9e8e2cc2149b9a09792fdd49923f0c
SriChandan2498 <chandan.zna7@gmail.com>
2021-08-16 13:27:43 +0530

    passed

--- 18-lineintersection-Python/lineintersection.py
+++ 18-lineintersection-Python/lineintersection.py
@@ -3,7 +3,13 @@
 #     y = m1*x + b1
 #     y = m2*x + b2
 # This function returns the x value of the point of intersection of the two lines. If the lines are parallel, or identical, the function should return None.
+def equals(a,b):
+	return abs(a-b) < 10**-9
 
 def lineintersection(m1, b1, m2, b2):
 	# your code goes here
-	pass
+	if(m1 == m2 or equals(m1/m2,b1/b2)):
+		return None
+	else:
+		return (b2-b1)//(m1-m2)
+	
