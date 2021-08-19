55809e32419d5625e319ce392797ac21321055c6
SriChandan2498 <chandan.zna7@gmail.com>
2021-08-19 11:11:21 +0530

    partially passed test cases

--- 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
+++ 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
@@ -3,8 +3,39 @@
 # 1, and nthPowerfulNumber(10) returns 64.
 # A number n is said to be Powerful Number if for every prime factor p of it, p2 also divides it. 
 # For example:- 36 is a powerful number. It is divisible by both 3 and square of 3 i.e, 9.
-
+import math
+def getprimefactors(n):
+	primefactors = list()
+	# if(n%2 == 0):
+	# 	primefactors.add(2)
+	while(n%2 == 0):
+		primefactors.append(2)
+		n = n // 2
+	for i in range(3,int(math.sqrt(n))+1,2):
+		if(n%i == 0):
+			primefactors.append(i)
+			n = n//i
+	if(n>2):
+		primefactors.append(n)
+	return primefactors
+def ispowerfulnumber(n):
+	primefactors = set(getprimefactors(n))
+	# print(primefactors)]
+	for i in primefactors:
+		if(n % (i)**2 != 0):
+			return False
+	return True
 
 def nthpowerfulnumber(n):
 	# Your code goes here
-	pass
+	if(n == 0):
+		return 1
+	i = 1
+	count = 0
+	while(True):
+		if(ispowerfulnumber(i)):
+			count += 1
+		if(count == n):
+			return i
+		i += 1
+print(nthpowerfulnumber(53)) 
