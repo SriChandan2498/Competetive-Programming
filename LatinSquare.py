d44ebac950db6049c35e198561d93dd7cc88ded2
SriChandan2498 <chandan.zna7@gmail.com>
2021-09-02 06:29:07 +0530

    passed

--- LatinSquare.py
+++ LatinSquare.py
@@ -9,4 +9,27 @@
 
 def isLatinSquare(lst):
     # Your code goes here...
-    pass
+    d = dict()
+    for i in lst:
+        temp = i
+        for j in range(len(lst[0])):
+            temp2 = i[:j] + i[j+1:]
+            if (i[j] in temp2):
+                return False
+            if j in d:
+                if(i[j] in d[j]):
+                    return False
+                else:
+                    d[j].append(i[j])
+            else:
+                d[j] = [i[j]]
+    return True
+
+def testlatinsquares():
+    assert isLatinSquare([['A','B','C'],['C','A','B'],['B','C','A']])==True
+    assert isLatinSquare([[1,2,3],[2,3,1],[3,1,2]])==True
+    assert isLatinSquare([['A','B','C'],['C','A','B'],['A','C','B']])==False
+    assert isLatinSquare([['A','B'],['A','A']])==False
+    print("All testcases passed")
+
+testlatinsquares()
