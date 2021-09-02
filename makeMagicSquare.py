fa692dbf6b9f15d161236562a8fe655fe6b73d85
SriChandan2498 <chandan.zna7@gmail.com>
2021-09-02 07:57:10 +0530

    passed

--- makeMagicSquare.py
+++ makeMagicSquare.py
@@ -35,4 +35,34 @@
 
 def makeMagicSquare(n):
     # Your code goes here...
-    pass
+    l = [[0 for x in range(n)] for y in range(n)]
+    i = int(n/2)
+    j = int(n-1)
+    number = 1
+    while number <= (n*n):
+        if(i<0 and j == n):
+            j = n - 2
+            i = 0
+        if(i < 0):
+            i = n - 1
+        if(j == n):
+            j = 0
+        if(l[i][j]!=0):
+            j -= 2
+            i += 1
+            continue
+        l[i][j] = number
+        number += 1
+        j = j + 1
+        i = i - 1
+    return l
+
+def test():
+    # Test Cases
+    assert makeMagicSquare(3)==[[2, 7, 6], [9, 5, 1], [4, 3, 8]], "test case failed"
+    assert makeMagicSquare(5)==[[9, 3, 22, 16, 15], [2, 21, 20, 14, 8], [25, 19, 13, 7, 1], [18, 12, 6, 5, 24], [11, 10, 4, 23, 17]], "test case failed"
+    assert makeMagicSquare(7)==[[20, 12, 4, 45, 37, 29, 28], [11, 3, 44, 36, 35, 27, 19], [2, 43, 42, 34, 26, 18, 10], [49, 41, 33, 25, 
+    17, 9, 1], [40, 32, 24, 16, 8, 7, 48], [31, 23, 15, 14, 6, 47, 39], [22, 21, 13, 5, 46, 38, 30]], "test case failed"
+    print("All testscases passed")
+
+test()
