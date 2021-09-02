152dace63a01d1f18f708eac7c74422bd85060c7
SriChandan2498 <chandan.zna7@gmail.com>
2021-09-02 08:22:39 +0530

    passed all test cases

--- 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
+++ 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
@@ -3,39 +3,39 @@
 # 1, and nthPowerfulNumber(10) returns 64.
 # A number n is said to be Powerful Number if for every prime factor p of it, p2 also divides it. 
 # For example:- 36 is a powerful number. It is divisible by both 3 and square of 3 i.e, 9.
-import math
-def getprimefactors(n):
-	primefactors = list()
-	# if(n%2 == 0):
-	# 	primefactors.add(2)
-	while(n%2 == 0):
-		primefactors.append(2)
-		n = n // 2
-	for i in range(3,int(math.sqrt(n))+1,2):
-		if(n%i == 0):
-			primefactors.append(i)
-			n = n//i
-	if(n>2):
-		primefactors.append(n)
-	return primefactors
-def ispowerfulnumber(n):
-	primefactors = set(getprimefactors(n))
-	# print(primefactors)]
-	for i in primefactors:
-		if(n % (i)**2 != 0):
+def isPrime(n):
+	if(n<2):
+		return False
+	if(n == 2):
+		return True
+	if(n%2==0):
+		return False
+	maxfactor = round(n**0.5)
+	for i in range(3,maxfactor+1,2):
+		if(n%i==0):
 			return False
 	return True
 
+def powerfulnumber(n):
+    l = []
+    for i in range(n):
+        if(isPrime(i) and n%i==0):
+            l.append(i)
+    if(len(l)==0):
+        return False
+    for i in l:
+        if n%(i**2)!=0:
+            return False
+    return True
+
 def nthpowerfulnumber(n):
 	# Your code goes here
-	if(n == 0):
+	if(n==0):
 		return 1
-	i = 1
-	count = 0
-	while(True):
-		if(ispowerfulnumber(i)):
-			count += 1
-		if(count == n):
-			return i
-		i += 1
-print(nthpowerfulnumber(53)) 
+	found = 0
+	guess = 0
+	while found < n:
+		guess += 1
+		if(powerfulnumber(guess)):
+			found += 1
+	return guess
